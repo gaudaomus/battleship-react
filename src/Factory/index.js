143e831292f2f42dd573ec8c3220510711c951bb
Jason Wang <75740057+gaudaomus@users.noreply.github.com>
2021-04-18 20:33:48 -0700

    first commit, copied over factory functions from battleship

---
+++ src/Factory/index.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const Ship = () => {
+  let position = [];
+  let positionHit = [];
+
+  const setPosition = (coord) => {
+    for (let i = 0; i < coord.length; i++) {
+      position[i] = coord[i];
+    }
+  };
+
+  const hit = (target) => {
+    if (position.indexOf(target) >= 0 && !(positionHit.indexOf(target) >= 0)) {
+      positionHit.push(target);
+    }
+  };
+
+  const isSunk = () => {
+    if (positionHit.length === position.length) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return { position, positionHit, setPosition, hit, isSunk };
+};
+
+const Gameboard = () => {
+  let shipOne = Ship();
+  let shipTwo = Ship();
+  let shipThree = Ship();
+  let shipFour = Ship();
+  let shipFive = Ship();
+
+  let allShips = [shipOne, shipTwo, shipThree, shipFour, shipFive];
+  let missedShots = [];
+  let hitShots = [];
+
+  const receiveAttack = (shot) => {
+    let misses = 0;
+    for (let i = 0; i < allShips.length; i++) {
+      if (allShips[i].position.indexOf(shot) >= 0) {
+        allShips[i].hit(shot);
+        hitShots.push(shot);
+      } else {
+        misses += 1;
+      }
+    }
+    if (misses === allShips.length) {
+      missedShots.push(shot);
+    }
+  };
+
+  const allSunk = () => {
+    let sunk = 0;
+    for (let i = 0; i < allShips.length; i++) {
+      if (allShips[i].isSunk() === false) {
+        break;
+      } else {
+        sunk += 1;
+      }
+    }
+    if (sunk === allShips.length) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return {
+    hitShots,
+    missedShots,
+    receiveAttack,
+    allSunk,
+    shipOne,
+    shipTwo,
+    shipThree,
+    shipFour,
+    shipFive,
+  };
+};
+
+const Player = (enemyBoard) => {
+  let activeTurn = false;
+
+  const fireShot = (shot) => {
+    if (
+      activeTurn === true &&
+      !(enemyBoard.hitShots.indexOf(shot) >= 0) &&
+      !(enemyBoard.missedShots.indexOf(shot) >= 0)
+    ) {
+      enemyBoard.receiveAttack(shot);
+    } else {
+      fireShot();
+    }
+  };
+
+  return { fireShot, activeTurn };
+};
+
+export { Ship, Gameboard, Player };
---
+++ src/Factory/index.test.js
@@ -0,0 +1,32 @@
+import { Ship, Gameboard } from "./index";
+
+test("all ships sunk", () => {
+  const myBoard = Gameboard();
+  myBoard.shipOne.setPosition([1, 2, 3]);
+  myBoard.shipTwo.setPosition([4, 5, 6]);
+  myBoard.shipThree.setPosition([7, 8, 9]);
+  myBoard.shipFour.setPosition([10, 11, 12]);
+  myBoard.shipFive.setPosition([13, 14, 15]);
+  for (let i = 0; i < 20; i++) {
+    myBoard.receiveAttack(i);
+  }
+  expect(myBoard.allSunk()).toBe(true);
+});
+
+test("not all ships sunk", () => {
+  const myBoard = Gameboard();
+  myBoard.shipOne.setPosition([1, 2, 3]);
+  myBoard.shipTwo.setPosition([4, 5, 6]);
+  myBoard.shipThree.setPosition([7, 8, 9]);
+  myBoard.shipFour.setPosition([10, 11, 12]);
+  myBoard.shipFive.setPosition([13, 14, 15]);
+  myBoard.receiveAttack(1);
+  myBoard.receiveAttack(2);
+  myBoard.receiveAttack(3);
+  myBoard.receiveAttack(4);
+  myBoard.receiveAttack(6);
+  myBoard.receiveAttack(7);
+  myBoard.receiveAttack(17);
+  expect(myBoard.missedShots).toStrictEqual([17]);
+  expect(myBoard.allSunk()).toBe(false);
+});
